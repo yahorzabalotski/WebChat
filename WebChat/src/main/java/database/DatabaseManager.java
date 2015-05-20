b20540bd48c99682ed3038db9bf13720d828fe7a
yahorzabolotsky <yahorzabolotsky@gmail.com>
2015-05-20 21:14:28 +0300

    add db class

---
+++ WebChat/src/main/java/database/DatabaseManager.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseManager {
+    private static final String URL = "jdbc:mysql://localhost:3306/todos";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "G05031996ggg";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+}
