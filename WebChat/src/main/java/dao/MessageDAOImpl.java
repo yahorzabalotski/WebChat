032d612a2224b145e71fd2d47707eb6410276960
yahorzabolotsky <yahorzabolotsky@gmail.com>
2015-05-22 21:26:36 +0300

    override selectById() method in MessageDAOImpl.java

--- WebChat/src/main/java/dao/MessageDAOImpl.java
+++ WebChat/src/main/java/dao/MessageDAOImpl.java
@@ -1,7 +1,20 @@
+
+import java.lang.Exception;
+import java.lang.Integer;
+import java.lang.System;
 import java.util.List;
+import java.util.Date;
 import java.lang.Override;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
+
 public class MessageDAOImpl implements MessageDAO {
+    private static Logger logger = Logger.getLogger(MessageDAOImpl.class.getName());
 
     @Override
     public void add(Message message){
@@ -13,11 +26,25 @@ public class MessageDAOImpl implements MessageDAO {
 
     @Override
     public void delete(Integer id){
+
     }
 
     @Override
     public Message selectById(Integer id){
-        return null;
+        Message message = null;
+
+        try(Connection connection = DatabaseManager.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM messages WHERE id = ?;")){
+            preparedStatement.setInt(1, id);
+            try(ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    message = new Message(resultSet.getInt("id"), resultSet.getInt("user_id"), resultSet.getString("text"), resultSet.getDate(3));
+                }
+            }
+        } catch (SQLException e) {
+            logger.error(e);
+        }
+        return message;
     }
 
     @Override
