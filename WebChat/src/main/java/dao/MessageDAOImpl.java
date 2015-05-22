0bf74b118c2397ef0371bfb37c6e84459e0e5404
yahorzabolotsky <yahorzabolotsky@gmail.com>
2015-05-22 18:16:59 +0300

    add MessageDAOImpl.java sceleton

---
+++ WebChat/src/main/java/dao/MessageDAOImpl.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.lang.Override;
+
+public class MessageDAOImpl implements MessageDAO {
+
+    @Override
+    public void add(Message message){
+    }
+
+    @Override
+    public void update(Message message){
+    }
+
+    @Override
+    public void delete(Integer id){
+    }
+
+    @Override
+    public Message selectById(Integer id){
+        return null;
+    }
+
+    @Override
+    public List<Message> selectAfter(Integer id){
+        return null;
+    }
+}
