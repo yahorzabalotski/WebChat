e4ba3c0bdb4fb1136ba91f3fae2e10ede6f8499b
yahorzabolotsky <yahorzabolotsky@gmail.com>
2015-05-23 10:11:34 +0300

    add UserDAOImpl.java skeleton

---
+++ WebChat/src/main/java/dao/UserDAOImpl.java
@@ -0,0 +1,30 @@
+import java.lang.Override;
+import java.util.List;
+
+public class UserDAOImpl implements UserDAO {
+
+    @Override
+    public void add(User user){
+
+    }
+
+    @Override
+    public void update(User user){
+
+    }
+
+    @Override
+    public void delete(Integer id){
+
+    }
+
+    @Override
+    public User selectById(Integer id){
+        return null;
+    }
+
+    @Override
+    public List<User> selectAfter(Integer id){
+        return null;
+    }
+}
